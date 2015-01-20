bf7dafa19915859445a8f6a8cd341bf4d522f408
Lea Marolt <maroltso@grinnell.edu>
2015-01-20 16:08:13 -0600

    Initial Commit

---
+++ Quoted/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quoted
+//
+//  Created by Lea Marolt Sonnenschein on 1/20/15.
+//  Copyright (c) 2015 Hellosunschein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quoted/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quoted
+//
+//  Created by Lea Marolt Sonnenschein on 1/20/15.
+//  Copyright (c) 2015 Hellosunschein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
